e6fed48fa0b982c09bea7fcea49ab98b2dc468ae
Antoine Groudiev <agroudiev@seas.harvard.edu>
2025-06-03 11:02:57 -0400

    test(iterative): add non-scaled test for TF16

--- test/iterative_TF16_test.hpp
+++ test/iterative_TF16_test.hpp
@@ -19,7 +19,8 @@ void generateAndProject(int n,
                         double* dA,
                         double* dA_psd,
                         cusolverDnHandle_t solverH,
-                        cublasHandle_t cublasH) {
+                        cublasHandle_t cublasH,
+                        double radius = 1.0) {
     size_t nn = size_t(n)*n;
     // 1) Generate random Gaussian M_h on host
     std::vector<double> M_h(nn);
@@ -47,7 +48,7 @@ void generateAndProject(int n,
     std::vector<double> lambda_h(n);
 
     // uniform eigenvalue
-    for(int i=0;i<n;i++) lambda_h[i] = -1.0 + 2.0*i/(double)(n-1);
+    for(int i=0;i<n;i++) lambda_h[i] = -radius + 2.0*i*radius/(double)(n-1);
 
     // 1/k eigenvalue
     // for (int i = 0; i < n; i++) {
@@ -148,7 +149,7 @@ void generateAndProject(int n,
     cudaFree(dLambda); cudaFree(dW); cudaFree(dWork_ev);
 }
 
-TEST(IterativeTF16, Random)
+TEST(IterativeTF16, RandomScaled)
 {
     cusolverDnHandle_t solverH; cublasHandle_t cublasH;
     CHECK_CUSOLVER(cusolverDnCreate(&solverH));
@@ -163,6 +164,40 @@ TEST(IterativeTF16, Random)
     CHECK_CUDA(cudaMalloc(&dA_psd, nn*sizeof(double)));
 
     generateAndProject(n, dA, dA_psd, solverH, cublasH); // cuSOLVER
+    projection_TF16(cublasH, dA, n, 0, 100); // iterative TF16 projection
+
+    // check if dA and dA_psd are approximately equal
+    double *dDiff; CHECK_CUDA(cudaMalloc(&dDiff, nn*sizeof(double)));
+    double one = 1.0, neg1 = -1.0;
+    CHECK_CUBLAS(cublasDgeam(
+        cublasH,
+        CUBLAS_OP_N, CUBLAS_OP_N,
+        n, n,
+        &one,  dA_psd, n,
+        &neg1, dA, n,
+        dDiff,       n));
+    double final_err = 0.0f;
+    CHECK_CUBLAS(cublasDnrm2(cublasH, nn, dDiff, 1, &final_err));
+
+    ASSERT_LE(final_err, 1e-2) << "Final error: " << final_err;
+}
+
+TEST(IterativeTF16, RandomNonScaled)
+{
+    cusolverDnHandle_t solverH; cublasHandle_t cublasH;
+    CHECK_CUSOLVER(cusolverDnCreate(&solverH));
+    CHECK_CUBLAS(cublasCreate(&cublasH));
+    CHECK_CUBLAS(cublasSetMathMode(cublasH, CUBLAS_TENSOR_OP_MATH));
+
+    size_t n = 1024;
+    size_t nn = n*n;
+
+    double *dA, *dA_psd;
+    CHECK_CUDA(cudaMalloc(&dA, nn*sizeof(double)));
+    CHECK_CUDA(cudaMalloc(&dA_psd, nn*sizeof(double)));
+
+    // we generate a random matrix with values in [-10/n, 10/n]
+    generateAndProject(n, dA, dA_psd, solverH, cublasH, 10.0); // cuSOLVER
     projection_TF16(cublasH, dA, n, 0, 100); // iterative TF16 projection
 
     // check if dA and dA_psd are approximately equal
